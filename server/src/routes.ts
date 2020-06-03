5a687abaf407075e18104025ab3524832a71642e
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-06-03 14:39:42 -0300

    feat: Create knex and database config

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const routes = express.Router();
 
-routes.get("/", (request: Request, response: Response) => {
-  return response.json({ message: " Hello world" });
+routes.get('/', (request: Request, response: Response) => {
+  return response.json({ message: ' Hello world' });
 });
 
 export default routes;
