51cf6961c65c16430ca73f1ecf9ccf5e1e5fc575
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-06-06 10:20:06 -0300

    feat: Implement get data in api

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import api from "../../services/api";
 
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
 const CreatePoint: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    api.get("items").then((response) => {
+      setItems(response.data);
+    });
+  }, []);
+
   return (
     <div id="page-create-point">
       <header>
@@ -84,48 +99,12 @@ const CreatePoint: React.FC = () => {
           </legend>
 
           <ul className="items-grid">
-            <li>
-              <img
-                src="http://localhost:3333/uploads/lampadas.svg"
-                alt="Teste"
-              />
-              <span>Lâmpadas</span>
-            </li>
-            <li className="selected">
-              <img
-                src="http://localhost:3333/uploads/lampadas.svg"
-                alt="Teste"
-              />
-              <span>Lâmpadas</span>
-            </li>
-            <li>
-              <img
-                src="http://localhost:3333/uploads/lampadas.svg"
-                alt="Teste"
-              />
-              <span>Lâmpadas</span>
-            </li>
-            <li>
-              <img
-                src="http://localhost:3333/uploads/lampadas.svg"
-                alt="Teste"
-              />
-              <span>Lâmpadas</span>
-            </li>
-            <li>
-              <img
-                src="http://localhost:3333/uploads/lampadas.svg"
-                alt="Teste"
-              />
-              <span>Lâmpadas</span>
-            </li>
-            <li>
-              <img
-                src="http://localhost:3333/uploads/lampadas.svg"
-                alt="Teste"
-              />
-              <span>Lâmpadas</span>
-            </li>
+            {items.map((item) => (
+              <li key={item.id}>
+                <img src={item.image_url} alt={item.title} />
+                <span>Lâmpadas</span>
+              </li>
+            ))}
           </ul>
         </fieldset>
 
